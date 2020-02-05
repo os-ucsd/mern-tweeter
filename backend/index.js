06e8ecdf5827820122e5c1fbb507062e4cf3d981
Brandon Nicholas Foey <38939989+BrandonFoey@users.noreply.github.com>
2020-02-04 16:19:38 -0800

    Added top files

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,14 @@ const tweetRoute = require('./routes/tweets');
 const userRoute = require('./routes/users');
 
 const app = express();
-const path = require('path');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true})));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
-
 app.use('/users', userRoute);
 
 app.listen(3000);
